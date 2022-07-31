07908aafb9b0b19ff64f6ffe8a931bb69f195877
Vinzce Yadao <yadaovinzce@gmail.com>
2022-07-30 22:01:48 -0400

    Exercise google interview question

--- Google/solution.py
+++ Google/solution.py
@@ -16,13 +16,32 @@ should return True
 # This means once we find a common item we can return True
 # must have 2 inputs (2 arrays)
 
+# # Brute Force
+# def common_item(arr1, arr2) -> bool:
+#     for item in arr1:       # O(N) looping through
+#         if item in arr2:    # O(M)
+#             return True
+#
+#     return False
+# # Time complexity = O(N * M)
+# # Space complexity = O(1)
 
+
+# FASTER METHOD
 def common_item(arr1, arr2) -> bool:
-    for item in arr1:       # O(N) looping through
-        if item in arr2:    # O(M)
+    # dictionary = {}       # O(N) space
+    # for element in arr1:        # O(N) time
+    #     dictionary[element] = 1
+
+    dictionary = {element: 1 for element in arr1}       # Still O(N) but list comprehension makes code more compact
+
+    for element in arr2:        # O(M) time
+        if dictionary.get(element) == 1:      # O(1) time
             return True
 
     return False
+# Time complexity = O(N + M)
+# Space complexity = Max(N, M)
 
 
 test_arr1 = ['a', 'b', 'c', 'x']
@@ -31,10 +50,9 @@ test_arr2 = ['z', 'y', 'i']
 test_arr3 = ['a', 'b', 'c', 'x']
 test_arr4 = ['z', 'y', 'x']
 
+test_arr5 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+test_arr6 = [11, 22, 33, 44, 55, 6, 7, 88, 9]
+
 print(common_item(test_arr1, test_arr2))
 print(common_item(test_arr3, test_arr4))
-
-# Time complexity = O(N * M)
-# Space complexity = O(1)
-
-# Faster method
+print(common_item(test_arr5, test_arr6))
